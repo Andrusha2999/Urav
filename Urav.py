27944cb1d4de87a6dea2a43b31568faa8b4a9190
opilane <opilane@HP-CZC6297B9K>
2022-01-11 11:57:58 +0200

    hjghjhg

--- Urav.py
+++ Urav.py
@@ -1,25 +1,25 @@
-from tkinter import*
-from math import*
-import matplotlib.pyplot as plt 
+from tkinter import *
+from math import *
+import matplotlib.pyplot as plt
 import numpy as np
 global a,b,c
-def lahenda():    
+def lahenda():
     if (a.get()!="" and b.get()!="" and c.get()!=""):
-        a_=int(a.get())
-        b_=int(b.get())
-        c_=int(c.get())
+        a_=float(a.get())
+        b_=float(b.get())
+        c_=float(c.get())
         D=b_*b_-4*a_*c_
         if D>0:
             x1_=round((-1*b_+sqrt(D))/(2*a_),2)
-            x2_=round((-1*b_-sqrt(D))/(2*a_),2)
+            x2_=round((-1*b_-sqrt(D))*(2*a_),2)
             t=f"X1={x1_}, \nX2={x2_}"
             graf=True
         elif D==0:
-            x1_=round((-1*b_)/(2*a_),2)
+            x1_round((-1*b_)/(2*a_),2)
             t=f"X1={x1_}"
             graf=True
         else:
-            t="Корней нет"
+            t="Нет корней"
             graf=False
         otvetik.configure(text=f"D={D}\n{t}")
         a.configure(bg="lightblue")
@@ -44,57 +44,135 @@ def graafik():
         x = np.arange(x0-10, x0+10, 0.5)
         y=a_*x*x+b_*x+c_
         fig = plt.figure()
-        plt.plot(x, y,'b:o', x0, y0,'g-d')
-        plt.title('Квадратное уравнение')
-        plt.ylabel('y')
-        plt.xlabel('x')
+        plt.plot(x, y,"b:o", x0, y0,"g-d")
+        plt.title("Квадратное уравнение")
+        plt.ylabel("y")
+        plt.xlabel("x")
         plt.grid(True)
         plt.show()
-        text=f"Вершина параболлы ({x0},{y0})"
+        text=f"вершина параболы({x0},{y0})"
     else:
-        text=f"График нет возможности построить"
+        text=f"график нет возможности построить"
     otvetik.configure(text=f"D={D}\n{t}\n{text}")
-
-
-
-
-
-
-
-
-
-
+t=0
+def veel():
+    global t
+    if t==0:
+        aken.geometry(str(aken.winfo_width())+"x"+str(aken.winfo_height()+200))
+        uvokno.config(text="Уменьшить окно")
+        t=1
+    else:
+        aken.geometry(str(aken.winfo_width())+"x"+str(aken.winfo_height()-200))
+        uvokno.config(text="Увеличить окно")
+        t=0
+def kit():
+    x1=np.arange(0,9,0.5)
+    y1=(2/27)*x1*x1-3
+    x2=np.arange(-10,0,0.5)
+    y2=0.04*x2*x2-3
+    x3=np.arange(-9,-3,0.5)
+    y3=(2/9)*(x3+6)**2+1
+    x4=np.arange(-3,9.5,0.5)
+    y4=(-1/12)*(x4-3)**2+6
+    x5=np.arange(5,9.5,0.5)
+    y5=(1/9)*(x5-5)**2+2
+    x6=np.arange(5,9,0.5)
+    y6=(1/8)*(x6-7)**2+1.5
+    x7=np.arange(-13,-8.5,0.5)
+    y7=(-0.75)*(x7+11)**2+6
+    x8=np.arange(-15,-12.5,0.5)
+    y8=(-0.5)*(x8+13)**2+3
+    x9=np.arange(-15,-10,0.5)
+    y9=[1]*len(x9)
+    x10=np.arange(3,4,0.5)
+    y10=[3]*len(x10)
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10)
+    plt.title("Квадратное уравнение")
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.grid(True)
+    plt.show()
+def o4ki():
+    x1=np.arange(-9,-1,0.5)
+    y1=(-1/16)*(x1+5)**2+2
+    x2=np.arange(1,9,0.5)
+    y2=(-1/16)*(x2-5)**2+2
+    x3=np.arange(-9,-1,0.5)
+    y3=1/4*(x3+5)**2-3
+    x4=np.arange(1,9,0.5)
+    y4=1/4*(x4-5)**2-3
+    x5=np.arange(-9,-6,0.5)
+    y5=-(x5+7)**2+5
+    x6=np.arange(6,9,0.5)
+    y6=-(x6-7)**2+5
+    x7=np.arange(-1,1,0.5)
+    y7=(-0.5)*x7*x7+1.5
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+    plt.title('Очки')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+def zont():
+    x1=np.arange(-12,12,0.5)
+    y1=(-1/18)*x1*x1+12
+    x2=np.arange(-4,4,0.5)
+    y2=(-1/8)*x2*x2+6
+    x3=np.arange(-12,-4,0.5)
+    y3=(-1/8)*(x3+8)**2+6
+    x4=np.arange(4,12,0.5)
+    y4=(-1/8)*(x4-8)**2+6
+    x5=np.arange(-4,-0.3,0.5)
+    y5=2*(x5+3)**2-9
+    x6=np.arange(-4,0.2,0.5)
+    y6=1.5*(x6+3)**2-10
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6)
+    plt.title('Зонтик')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
 aken=Tk()
-aken.title("Решение Уравнения")
-aken.geometry("750x280")
-reshen=Label(aken,text="Решение_Квадратного_Уравнения",font="Arial 20", fg="black",bg="lightyellow")
-otvetik=Label(aken,text="ответик", height=4,width=60,bg="purple")
-a=Entry(aken,font="Arial 25",bg="lightblue",    fg="green",width=4)
-x2=Label(aken,text="x**2+",  font="Arial 25",   fg="green", padx=12)
-b=Entry(aken,font="Arial 25",bg="lightblue",    fg="green",width=4)
-x=Label(aken,text="x+",      font="Arial 25",   fg="green")
-c=Entry(aken,font="Arial 25",bg="lightblue",    fg="green",width=5)
-piip=Label(aken,text="=0",   font="Arial 25",    fg="green")
-knopka=Button(aken,text="РЕШИТЬ",font="Arial 25",  bg="pink",command=lahenda)
-grafik_=Button(aken,text="График", font="Arial 25",bg="green",command=graafik)
-radio=Radiobutton(aken,text="кит",font="Arial 25", fg="blue",bg="lightpink") 
-radio2=Radiobutton(aken,text="зонтик",font="Arial 25", fg="blue",bg="lightpink") 
-radio3=Radiobutton(aken,text="лягушка",font="Arial 25", fg="blue",bg="lightpink") 
+aken.title("Решение квадратного уравнения")
+aken.geometry("800x300")
+f1=Frame(aken,width=650,height=200)
+f1.pack(side=TOP)
+f2=Frame(aken,width=650,height=200)
+f2.pack(side=BOTTOM)
+
+lk=Label(f1,text="Решение_квадратного_уравнения",font="Arial 25", fg="green",bg="yellow")
+otvetik=Label(f1,text="Решение", height=4,width=60,bg="blue")
+a=Entry(f1,font="Arial 25",fg="green",bg="lightblue",width=3)
+x2=Label(f1,text="x**2+",fg="green",font="Arial 25", padx=10)
+b=Entry(f1,font="Arial 25",fg="green",bg="lightblue",width=3)
+x=Label(f1,text="x+",fg="green",font="Arial 25")
+c=Entry(f1,font="Arial 25", fg="green",bg="lightblue",width=3)
+ghj=Label(f1,text="=0",font="Arial 25", fg="purple")
+reshka=Button(f1,text="Решить",font="Arial 25",bg="pink",command=lahenda)
+graafik=Button(f1,text="График", font="Arial 25",bg="red",command=graafik)
+uvokno=Button(f2,text="Увеличить окно", font="Arial 25",bg="green",command=veel)
+kala=Radiobutton(f2,text="Кит",font="Arial 25",bg="pink",command=kit)
+o4ki=Radiobutton(f2,text="очки",font="Arial 25",bg="blue",command=o4ki)
+zont=Radiobutton(f2,text="зонтик", font="Arial 20",bg="green",command=zont)
 
 
 
-reshen.pack()
+
+lk.pack()
 otvetik.pack(side=BOTTOM)
 a.pack(side=LEFT)
 x2.pack(side=LEFT)
 b.pack(side=LEFT)
 x.pack(side=LEFT)
 c.pack(side=LEFT)
-piip.pack(side=LEFT)
-knopka.pack(side=RIGHT)
-grafik_.pack(side=LEFT)
-radio.pack()
-radio2.pack()
-radio3.pack()
-
+ghj.pack(side=LEFT)
+reshka.pack(side=LEFT)
+graafik.pack(side=LEFT)
+uvokno.pack()
+kala.pack()
+o4ki.pack()
+zont.pack()
 aken.mainloop()
